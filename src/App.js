8619963fe1d3b38e3665e57587055f4241014439
soloveiiko <soloveymaria51@gmail.com>
2023-03-28 20:53:47 +0300

    CurrConverter: Added header and scss files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
+import "./styles/css/style.css";
 
 function App() {
   return (
-    <div className="main">
-    </div>
+    <Header />
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 class Header extends React.Component {
-
+    render() {
+        return (
+            <header className="header">
+                <div className="header__container _container" >
+                    <h1 className="header__logo">Currency Converter</h1>
+                </div>
+            </header>
+        )
+    }
 }
 export default Header
