6b92780a90ac649ec38213039700357a45d2129a
soloveiiko <soloveymaria51@gmail.com>
2023-04-08 12:39:53 +0300

    CurrConverter: Added official exchange rate to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-function Header() {
+function Header(props) {
     return (
         <header className="currency-converter__header header">
             <div className="header__container _container" >
                 <h1 className="header__logo">Currency Converter</h1>
                 <div className="header__text">The official exchange rate of the
                     hryvnia against foreign currencies</div>
-                {/* <div className="currency-body__currency-container currency">
-                        <div className="currency__dollar">{format(1 * rates.UAH / rates.USD)} USD</div>
-                        <div className="currency__euro">{format(1 * rates.UAH / rates.EUR)} EUR</div>
-                        <div className="currency__pound-sterling">{format(1 * rates.UAH / rates.GBP)} GBP</div>
-                    </div> */}
+                <div className="header__currency-container currency">
+                    <div className="currency__dollar">{props.format(1 * props.rates.UAH / props.rates.USD)} USD</div>
+                    <div className="currency__euro">{props.format(1 * props.rates.UAH / props.rates.EUR)} EUR</div>
+                    <div className="currency__pound-sterling">{props.format(1 * props.rates.UAH / props.rates.GBP)} GBP</div>
+                </div>
             </div>
         </header>
     )
